9246444f2c6cf29770037a19728428188bb1986b
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2022-08-02 20:27:21 +0200

    country -> jurisdiction

--- src/jurisdictions.jl
+++ src/jurisdictions.jl
@@ -1,18 +1,22 @@
 
 """
-AbstractJurisdiction
+    AbstractJurisdiction
 
 An abstract type representing an interface for a patent jurisdiction. 
 Concrete implementations, such as `PatentsLens.LensJurisdiction`, should subtype this.
 """
 abstract type AbstractJurisdiction end
 
-
-"""
-country(j::AbstractJurisdiction)
-
-return the country code of the jurisdiction `j`.
-"""
-function country(j::AbstractJurisdiction)::String
-    throw(ArgumentError("$(typeof(j)) does not contain country information."))
+function jurisdiction(a::AbstractApplication)::AbstractJurisdiction
+    throw(ArgumentError("$(typeof(a)) does not contain jurisdiction information."))
 end
+
+
+# """
+#     country(j::AbstractJurisdiction)
+
+# return the country code of the jurisdiction `j`.
+# """
+# function country(j::AbstractJurisdiction)::String
+#     throw(ArgumentError("$(typeof(j)) does not contain country information."))
+# end
