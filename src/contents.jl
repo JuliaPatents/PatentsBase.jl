72f5ad2610a9d128b9d25e3f58cece689cc7633f
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2022-08-03 08:18:19 +0200

    docstring indents

--- src/contents.jl
+++ src/contents.jl
@@ -1,5 +1,5 @@
 """
-AbstractTitle
+    AbstractTitle
 
 An abstract type representing an interface for the title(s) of a patent application. 
 Concrete implementations, such as `PatentsLens.LensTitle`, should subtype this.
@@ -8,7 +8,7 @@ abstract type AbstractTitle end
 
 
 """
-text(lang::String)
+    text(lang::String)
 
 Returns a `String` with the localized version of the title `t` for language `lang`.
 Concrete implementations should throw a `Base.KeyError` if there is no title for that locale.
